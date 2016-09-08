4bb76d39e24ff8d0d15443650c83533f93c005c8
Meyer McMains <meyer.mcmains@gmail.com>
2016-09-08 19:17:07 -0400

    Add project files.

---
+++ Granite/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Granite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.username = new System.Windows.Forms.TextBox();
+            this.pswd = new System.Windows.Forms.TextBox();
+            this.usrlabel = new System.Windows.Forms.Label();
+            this.pswdLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // username
+            // 
+            this.username.BackColor = System.Drawing.SystemColors.Window;
+            this.username.Location = new System.Drawing.Point(244, 213);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(120, 20);
+            this.username.TabIndex = 1;
+            // 
+            // pswd
+            // 
+            this.pswd.Location = new System.Drawing.Point(244, 259);
+            this.pswd.Name = "pswd";
+            this.pswd.PasswordChar = '*';
+            this.pswd.Size = new System.Drawing.Size(120, 20);
+            this.pswd.TabIndex = 2;
+            this.pswd.TextChanged += new System.EventHandler(this.pswd_TextChanged);
+            // 
+            // usrlabel
+            // 
+            this.usrlabel.AutoSize = true;
+            this.usrlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usrlabel.Location = new System.Drawing.Point(264, 190);
+            this.usrlabel.Name = "usrlabel";
+            this.usrlabel.Size = new System.Drawing.Size(83, 20);
+            this.usrlabel.TabIndex = 3;
+            this.usrlabel.Text = "Username";
+            // 
+            // pswdLabel
+            // 
+            this.pswdLabel.AutoSize = true;
+            this.pswdLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pswdLabel.Location = new System.Drawing.Point(264, 236);
+            this.pswdLabel.Name = "pswdLabel";
+            this.pswdLabel.Size = new System.Drawing.Size(78, 20);
+            this.pswdLabel.TabIndex = 4;
+            this.pswdLabel.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 418);
+            this.Controls.Add(this.pswdLabel);
+            this.Controls.Add(this.usrlabel);
+            this.Controls.Add(this.pswd);
+            this.Controls.Add(this.username);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox pswd;
+        private System.Windows.Forms.Label usrlabel;
+        private System.Windows.Forms.Label pswdLabel;
+    }
+}
+
---
+++ Granite/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Granite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            usrlabel.Left = (this.ClientSize.Width - username.Width) / 2;
+            usrlabel.Top = (this.ClientSize.Height - username.Height) / 2;
+            username.Left = this.usrlabel.Left;
+            username.Top = (this.usrlabel.Top + usrlabel.Height);
+            pswdLabel.Left = this.usrlabel.Left;
+            pswdLabel.Top = (this.username.Top + username.Height);
+            pswd.Left = this.usrlabel.Left;
+            pswd.Top = (this.pswdLabel.Top + pswd.Height);
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            usrlabel.Left = (this.ClientSize.Width - username.Width) / 2;
+            usrlabel.Top = (this.ClientSize.Height - username.Height) / 2;
+            username.Left = this.usrlabel.Left;
+            username.Top = (this.usrlabel.Top + usrlabel.Height);
+            pswdLabel.Left = this.usrlabel.Left;
+            pswdLabel.Top = (this.username.Top + username.Height);
+            pswd.Left = this.usrlabel.Left;
+            pswd.Top = (this.pswdLabel.Top + pswd.Height);
+        }
+
+        private void pswd_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
