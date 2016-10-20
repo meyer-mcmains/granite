0d2a2d67531f5a442283f464207e5f5ec8e92ae1
humannt <humann@etsu.edu>
2016-10-20 18:36:44 -0400

    Add files via upload

---
+++ createandgivetest/Granite/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Granite
+{
+    public partial class Form1 : Form
+    {
+        private Connection c;
+        public Form1()
+        {
+            InitializeComponent();
+            ConnectDatabase();
+        }
+
+        //center login labels and txtboxes on load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            usrlabel.Left = (this.ClientSize.Width - username.Width)/2;
+            usrlabel.Top = (this.ClientSize.Height - username.Height)/2;
+            username.Left = this.usrlabel.Left;
+            username.Top = (this.usrlabel.Top + usrlabel.Height);
+            pswdLabel.Left = this.usrlabel.Left;
+            pswdLabel.Top = (this.username.Top + username.Height);
+            pswd.Left = this.usrlabel.Left;
+            pswd.Top = (this.pswdLabel.Top + pswd.Height);
+            loginButton.Left = this.usrlabel.Left;
+            loginButton.Top = (this.pswd.Top + pswd.Height);
+        }
+
+        //recenter login labels and txtboxes if window is resized
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            usrlabel.Left = (this.ClientSize.Width - username.Width)/2;
+            usrlabel.Top = (this.ClientSize.Height - username.Height)/2;
+            username.Left = this.usrlabel.Left;
+            username.Top = (this.usrlabel.Top + usrlabel.Height);
+            pswdLabel.Left = this.usrlabel.Left;
+            pswdLabel.Top = (this.username.Top + username.Height);
+            pswd.Left = this.usrlabel.Left;
+            pswd.Top = (this.pswdLabel.Top + pswd.Height);
+            loginButton.Left = this.usrlabel.Left;
+            loginButton.Top = (this.pswd.Top + pswd.Height);
+        }
+
+        //establish a connection to the database
+        private void ConnectDatabase()
+        {
+             
+
+            try
+            {
+                c = new Connection();
+                indicator.ForeColor = Color.Green;
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                indicator.ForeColor = Color.Red;
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            Connection c = new Connection();
+            string usrName = null;
+            string password = null;
+            try
+            {               
+                indicator.ForeColor = Color.Green;
+
+                MySqlDataReader reader = null;
+                string selectUser = "SELECT username FROM user";
+
+                MySqlCommand getUsername = new MySqlCommand(selectUser, c.getConn());
+                reader = getUsername.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    usrName = (string) reader["username"];
+                }
+                reader.Close();
+
+                string selectPassword = "SELECT password FROM user";
+
+                MySqlCommand getPassword = new MySqlCommand(selectPassword, c.getConn());
+                reader = getPassword.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    password = (string) reader["password"];
+                }
+                reader.Close();
+
+                if (username.Text == usrName)
+                {
+                    if (pswd.Text == password)
+                    {
+
+                        c.Close();
+                        User u = new User(username.Text);
+                        Home hw = new Home();
+                        hw.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Incorrect Password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Username Not Found","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                indicator.ForeColor = Color.Red;
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            c.Close();
+            Application.Exit();
+        }
+    }
+}
---
+++ createandgivetest/Granite/makeTest.cs
@@ -0,0 +1,95 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Granite
+{
+    public partial class makeTest : Form
+    {
+        private Connection conn;
+        public makeTest()
+        {
+            InitializeComponent();
+            ConnectDatabase();
+            populateCombo();
+        }
+
+        public void ConnectDatabase()
+        {
+            try
+            {
+                conn = new Connection();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void makeTest_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveQuestion_Click(object sender, EventArgs e)
+        {
+            MySqlDataReader rdr = null;
+            try
+            {
+                string strCourse = comboBox2.SelectedItem.ToString();
+                string strAdmin = "admin";
+                strCourse = strCourse.Substring(0, 4);
+                //richTextBox1.Text = strCourse;
+                string strQuery = "INSERT INTO question(questiontext, answertext, creator, course) VALUES('"+richTextBox1.Text+"','" + richTextBox2.Text+"','"+strAdmin+"',"+ strCourse+")";
+                MySqlCommand addQuestion = new MySqlCommand(strQuery, conn.getConn());
+                rdr = addQuestion.ExecuteReader();
+                rdr.Close();
+                richTextBox1.Text = "";
+                richTextBox2.Text = "";
+            }
+            catch(Exception ex)
+            {
+
+            }
+            
+        }
+
+        private void finishCreating_Click(object sender, EventArgs e)
+        {
+            Home h = new Home();
+            h.Show();
+            this.Hide();
+        }
+
+
+
+        private void populateCombo()
+        {
+            List<string> list = new List<string>();
+            MySqlDataReader reader = null;
+            string selectCourse = "SELECT * FROM course";
+
+            MySqlCommand getCourse = new MySqlCommand(selectCourse, conn.getConn());
+            reader = getCourse.ExecuteReader();
+            
+            while (reader.Read())
+            {
+                //String test = (string)reader["name"];
+                //label3.Text = (string)reader["name"];
+
+                this.comboBox2.Items.Add(reader["courseID"].ToString() + " " + (string)reader["name"]);
+                if (!reader.HasRows)
+                    break;
+            }
+            reader.Close();
+            return;
+        }
+    }
+}
