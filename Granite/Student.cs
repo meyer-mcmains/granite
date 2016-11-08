12a29e65638c01ebc2a68932e691927f179ea8d4
Midget <Midget@TARDIS-HP>
2016-11-08 16:52:12 -0500

    fixed

--- Granite/Student.cs
+++ Granite/Student.cs
@@ -12,12 +12,22 @@ namespace Granite
         internal string first { get; set; }
         internal string last { get; set; }
         internal bool testTaken { get; set; }
+        internal string concentration { get; set; }
 
         public Student(string e, string f, string l)
         {
             this.enumber = e;
             this.first = f;
             this.last = l;
+            testTaken = false;
+        }
+
+        public Student(string e, string f, string l, string c)
+        {
+            this.enumber = e;
+            this.first = f;
+            this.last = l;
+            this.concentration = c;
             testTaken = false;
         }
     }
---
+++ Granite/addStudent.cs
@@ -0,0 +1,108 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Granite
+{
+    public partial class addStudent : Form
+    {
+
+        private Connection conn;
+        private Student s;
+
+        private int eChanged;
+        private int firstChanged;
+        private int lastChanged;
+        private int conChanged;
+
+        public addStudent()
+        {
+            InitializeComponent();
+            this.eNumberTxt.KeyPress += new KeyPressEventHandler(eNumberTxt_KeyPress);
+            CreateStudent();
+        }
+        
+        private void CreateStudent()
+        {
+            concentratoinCombo.Items.Add("CS");
+            concentratoinCombo.Items.Add("IT");
+        }
+
+        private void eNumberTxt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !(char.IsDigit(e.KeyChar) || e.KeyChar == 8);
+        }
+
+        private void eNumberTxt_TextChanged(object sender, EventArgs e)
+        {
+            eChanged = 1;
+            checkedChanged();
+        }
+
+        private void firstTxt_TextChanged(object sender, EventArgs e)
+        {
+            firstChanged = 1;
+            checkedChanged();
+        }
+
+        private void lastTxt_TextChanged(object sender, EventArgs e)
+        {
+            lastChanged = 1;
+            checkedChanged();
+        }
+
+        private void concentratoinCombo_TextChanged(object sender, EventArgs e)
+        {
+            conChanged = 1;
+            checkedChanged();
+        }
+
+        private void checkedChanged()
+        {
+            if(eChanged == 1 && firstChanged == 1 && lastChanged == 1 && conChanged == 1)
+            {
+                submit.Visible = true;
+                submit.Enabled = true;
+                s = new Student(eNumberTxt.Text, firstTxt.Text, lastTxt.Text, concentratoinCombo.Text);
+            }
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            conn = new Connection();
+
+            try
+            {
+                string inStudent = "INSERT INTO student (enum, firstname, lastname, concentration) VALUES (@enumber, @first, @last, @con);";
+
+                using (MySqlCommand cmd = new MySqlCommand(inStudent, conn.getConn()))
+                {
+                    cmd.Parameters.AddWithValue("@enumber", s.enumber);
+                    cmd.Parameters.AddWithValue("@first", s.first);
+                    cmd.Parameters.AddWithValue("@last", s.last);
+                    cmd.Parameters.AddWithValue("@con", s.concentration);
+
+                    cmd.ExecuteNonQuery();
+                }
+                conn.Close();
+                Close();
+                
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            conn.Close();
+            Close();
+
+        }
+    }
+}
